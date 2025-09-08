370ad9beb901b1ec94be80e225525812c659b671
Farid Seifi <farid.seifi@gmail.com>
2025-09-08 10:26:35 -0400

    updating  CYP2D6 genotype to phenotype tests

--- collection/CPIC_Phenotype_CYP2D6/tests/test_service.py
+++ collection/CPIC_Phenotype_CYP2D6/tests/test_service.py
@@ -1,17 +1,18 @@
 from cpic_phenotype_cyp2d6 import phenotype
 
-def test_knowledge():
+
+def test_service():
     result = phenotype({"CYP2D6": "*3/*3"})
     assert result["CYP2D6"]["phenotype"] == "Poor metabolizer"
-    
+
     result = phenotype({"CYP2C19": "*3/*3"})
     assert result["CYP2D6"]["phenotype"] == "Unknown"
-    
-    result = phenotype({"CYP2D6": "*3/*3" , "CYP2C19": "*3/*3"})
+
+    result = phenotype({"CYP2D6": "*3/*3", "CYP2C19": "*3/*3"})
     assert result["CYP2D6"]["phenotype"] == "Poor metabolizer"
-    
+
     result = phenotype({"CYP2D6": "*1x2/*68"})
-    assert result["CYP2D6"]["phenotype"] == "Normal metabolizer"    
-    
+    assert result["CYP2D6"]["phenotype"] == "Normal metabolizer"
+
     result = phenotype({"CYP2D6": "*9/*96"})
-    assert result["CYP2D6"]["phenotype"] == "Intermediate metabolizer"       
+    assert result["CYP2D6"]["phenotype"] == "Intermediate metabolizer"
