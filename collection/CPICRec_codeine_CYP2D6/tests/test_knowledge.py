ce582f8218764b861891179c6bed35d4ed14cd50
Farid Seifi <farid.seifi@gmail.com>
2025-09-08 13:33:17 -0400

    Updating coedinde CYP2D6 tests and metadata

--- collection/CPICRec_codeine_CYP2D6/tests/test_knowledge.py
+++ collection/CPICRec_codeine_CYP2D6/tests/test_knowledge.py
@@ -1,11 +1,27 @@
 from cpicrec_codeine_cyp2d6 import dosingrecommendation
 
-def test_knowledge():
-    result = dosingrecommendation({'CYP2D6': {'diplotype': '*3/*3', 'phenotype': 'Poor metabolizer'}})
-    assert "Avoid codeine use due to lack of efficacy." in result["recommendation"]["content"]    
-    assert "Greatly reduced morphine formation following codeine administration" in result["recommendation"]["implication"]  
+def test_cyp2d6ultrarapid():
+    result = dosingrecommendation({'CYP2D6': {'phenotype': 'Ultrarapid metabolizer'}})
+    assert "Avoid codeine use due to potential for toxicity." in result["recommendation"]["content"]    
+    assert result["recommendation"]["implication"] == "Increased formation of morphine following codeine administration, leading to higher risk of toxicity" 
+    assert result["recommendation"]["classification"] == "Strong"
+    
+def test_cyp2d6normal():
+    result = dosingrecommendation({'CYP2D6': {'phenotype': 'Normal metabolizer'}})
+    assert result["recommendation"]["content"] == "Use label-recommended age- or weight-specific dosing."    
+    assert result["recommendation"]["implication"] == "Normal morphine formation" 
+    assert result["recommendation"]["classification"] == "Strong"
 
-    result = dosingrecommendation({'CYP2D6': {'diplotype': '*3/*48', 'phenotype': 'Normal metabolizer'}})
-    assert result["recommendation"]["content"] == "Use label-recommended age- or weight-specific dosing."  
-    assert result["recommendation"]["implication"] == "Normal morphine formation"
-       
+def test_cyp2d6intermediate():
+    result = dosingrecommendation({'CYP2D6': {'phenotype': 'Intermediate metabolizer'}})
+    assert result["recommendation"]["content"] == "Use label-recommended age- or weight-specific dosing. If no response, consider alternative analgesics such as morphine or a nonopioid. Monitor tramadol use for response."    
+    assert result["recommendation"]["implication"] == "Reduced morphine formation" 
+    assert result["recommendation"]["classification"] == "Moderate"
+    
+def test_cyp2d6poor():
+    result = dosingrecommendation({'CYP2D6': {'phenotype': 'Poor metabolizer'}})
+    assert "Avoid codeine use due to lack of efficacy." in result["recommendation"]["content"]    
+    assert result["recommendation"]["implication"] == "Greatly reduced morphine formation following codeine administration, leading to insufficient pain relief." 
+    assert result["recommendation"]["classification"] == "Strong"
+    
+
---
+++ collection/CPICRec_codeine_CYP2D6/tests/test_service.py
@@ -0,0 +1,15 @@
+from cpicrec_codeine_cyp2d6 import dosingrecommendation
+
+def test_service():
+    result = dosingrecommendation({'CYP2D6': { 'phenotype': 'Poor metabolizer'}})
+    assert type(result) is dict
+    assert result.keys() == {'type', 'drug', 'genes', 'recommendation'}
+    assert "Avoid codeine use due to lack of efficacy." in result["recommendation"]["content"]    
+    assert "Greatly reduced morphine formation following codeine administration" in result["recommendation"]["implication"]  
+
+    result = dosingrecommendation({'CYP2D6': { 'phenotype': 'Unknown metabolizer'}})
+    assert result == "Incorrect/invalid input for drug Codeine"
+
+    result = dosingrecommendation({'CYP2D6': {'wrongKey': 'Poor metabolizer'}})
+    assert result == "Incorrect/invalid input."
+       
